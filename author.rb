174e4dfe8bfcbfbb7b8fcfe4d07f33eee1dc9825
Anas Abubakar <anasabubakar2015@gmail.com>
2022-10-12 15:30:15 +0100

    Create unit test for author class

--- author.rb
+++ author.rb
@@ -2,7 +2,7 @@ class Author
   attr_reader :id, :first_name, :last_name
   attr_accessor :items
 
-  def initialize(first_name, last_name)
+  def initialize(id, first_name, last_name)
     @id = id || Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
