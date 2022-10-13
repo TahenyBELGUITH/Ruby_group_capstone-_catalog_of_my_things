c47f9ca3e9d6e5114df92e22b166c45155fe5bcb
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-10-13 10:27:53 +0100

    Add RSpec test

--- data/data_read.rb
+++
@@ -1,9 +0,0 @@
-require 'json'
-
-def read_authors
-  return [] unless File.exist?('./storage/authors.json')
-
-  JSON.parse(File.read('./storage/authors.json')).map do |author|
-    Author.new(author['first_name'], author['last_name'], author['id'])
-  end
-end
--- data/data_write.rb
+++
@@ -1,8 +0,0 @@
-def save_author
-  authors = []
-  @authors.each do |author|
-    authors.push({ first_name: author.first_name, last_name: author.last_name, id: author.id })
-  end
-
-  File.write('./storage/authors.json', JSON.generate(authors))
-end
