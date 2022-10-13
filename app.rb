a170dac7c632e99b797ad028a67e307067d64664
Anas Abubakar <anasabubakar2015@gmail.com>
2022-10-13 15:22:47 +0100

    Modify app.rb file

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
 require_relative './classes/book_options'
 require_relative './classes/game_options'
+require_relative './classes/preserve_game'
 
 class App
   def initialize
     @books = []
-    @games = []
+    @preserve_game = PreserveGame.new
+    @preserve_game.load_authors
+    @preserve_game.load_games
     @options = [
       'List all books',
       'List all music albums',
@@ -38,10 +41,10 @@ class App
     case user_choice
     when 1 then list_all_books
     when 2 then list_all_music_albums
-    when 3 then list_all_games
+    when 3 then list_all_games(@preserve_game)
     when 4 then list_all_genres
     when 5 then list_all_labels
-    when 6 then list_all_authors
+    when 6 then list_all_authors(@preserve_game)
     else operations_two(user_choice)
     end
   end
@@ -50,7 +53,7 @@ class App
     case user_choice
     when 7 then add_book
     when 8 then add_music_album
-    when 9 then add_game
+    when 9 then add_game(@preserve_game)
     when 10 then exit_app
     else p 'Please choose a number between 1-10'
     end
